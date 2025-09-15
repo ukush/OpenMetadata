d5efb893b6af31ce03472fa827f221cccf18cfae
ukush <ukushi01@gmail.com>
2025-09-15 14:41:10 +0100

    reverted changes and check circle icons being used from project assets folder

--- openmetadata-ui/src/main/resources/ui/src/pages/Persona/PersonaDetailsPage/PersonaDetailsPage.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/Persona/PersonaDetailsPage/PersonaDetailsPage.tsx
@@ -10,7 +10,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-import { CheckCircleOutlined } from '@ant-design/icons';
 import Icon from '@ant-design/icons/lib/components/Icon';
 import { Button, Col, Modal, Row, Tabs, Typography } from 'antd';
 import { ItemType } from 'antd/lib/menu/hooks/useItems';
@@ -20,6 +19,7 @@ import { isUndefined } from 'lodash';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import { ReactComponent as CheckCircleOutlined } from '../../../assets/svg/check-circle-outline.svg';
 import { ReactComponent as CloseCircleOutlined } from '../../../assets/svg/close-circle-outlined.svg';
 import { ReactComponent as IconPersona } from '../../../assets/svg/ic-personas.svg';
 import DescriptionV1 from '../../../components/common/EntityDescription/DescriptionV1';
